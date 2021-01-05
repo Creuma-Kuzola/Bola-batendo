60799ef628220cf1c5015e86ab419b4dfc9eb119
creuma <creumakuzola@gmail.com>
2021-01-05 12:43:16 +0100

    Updates
    Adicionei o anti-aliasing que é o processo de tornar as bordas das linhas em formas ou texto menos recortadas, ou seja, mais suaves.

--- PainelBola.java
+++ PainelBola.java
@@ -9,6 +9,7 @@ import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
+import java.awt.RenderingHints;
 import java.awt.Toolkit;
 import java.awt.geom.Ellipse2D;
 import javax.swing.JPanel;
@@ -20,7 +21,7 @@ import javax.swing.JPanel;
 public class PainelBola extends JPanel implements Runnable{
     
     private final Thread thread;
-    int x= 0;
+    int x= 0,count=0, count1=5;
     int altura, largura;
     
     private double aceleracao = (double) -9.8;
@@ -29,9 +30,8 @@ public class PainelBola extends JPanel implements Runnable{
     private double vFinalYAntesdaQueda = 0;
     private double tempo = 0;
     private double espacoY=0;
-    private double y =0 ,velocidadeY = 9.8;
-    
-
+    private double y =0 ,velocidadeY = 9.8, aux=0;
+  
     /**
      *
      * @param g
@@ -50,6 +50,8 @@ public class PainelBola extends JPanel implements Runnable{
         super.paintComponent(g);
         Graphics2D g2d = (Graphics2D) g;
         
+        // Anti-Aliasing
+       g2d.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
         this.setBackground(new Color(255,255,255));
         Ellipse2D bola = new Ellipse2D.Double(x,y,50,50);
         g2d.setPaint(new Color(255,165,0));
@@ -67,35 +69,35 @@ public class PainelBola extends JPanel implements Runnable{
         System.out.println("Altura"+espacoY+ "\n" + "Velocidade Final: "+ vFinalYAntesdaQueda+ "\n" + 
         "tempo: " + tempo);
         
-        
     }
     
-   
     // Função responsável por movimentar a bola
     public void movimentar() {
     
-       // Subtraí 60 na altura para garantir que a bola não "afunda-se" ou desaparecesse da tela.
        largura = larguraDimensaoDaTela()/2-25;
-       altura = alturaDimensaoDaTela()/2 -70;
-       
+       altura = alturaDimensaoDaTela()/2 -70;  
        x = largura/2;
-      
-       System.out.println("Y: "+y);
-       
-      if(y < 0){
-            velocidadeY*=-1;  
+     
+       if(y < 0)
+         velocidadeY*=-1;  
+       else if(y > altura)
+       {  
+           aux = y;
+           System.out.println("aux: "+aux);
+           velocidadeY*=-1;
+           count=1;
        }
-       
-     else if(y > altura){
-            velocidadeY*=-1;
-           
-            System.out.println("Y: "+y);
-      }
        y +=velocidadeY;
+       
+       if(count == 1){
+           
+           aux = largura/ 2;
+           
+      
+       }
     
     }
 
-    
     // Funções para pegar a largura e a altura do screen 
      public int larguraDimensaoDaTela(){
         
